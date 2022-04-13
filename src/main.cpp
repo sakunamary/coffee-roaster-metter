f723be950d019597fc5374b2318e5c3d1b2c63f8
Tim <sakunamary@gmail.com>
2022-04-13 23:26:52 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 
 
 Adafruit_AS7341 as7341; // I2C address 0x39
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R3, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 5, /* data=*/ 4); 
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
 
 uint16_t readings[12]; //AS7341 data array
 float counts[12];
@@ -20,15 +23,16 @@ void show_open();
 
 void setup() {
 
-u8g2.begin();
-u8g2.setFont(u8g2_font_6x12_tr);
+u8g2.begin(); 
+u8g2.setFont(u8g2_font_6x10_tr);
+u8g2.firstPage();
+    do{
+        delay(500); 
+        u8g2.drawStr(0,24,"AS7341 Initing...");
+        }while( u8g2.nextPage());
 
-delay(2000); // Pause for 2 seconds
+delay(2000);
 
-  if (!as7341.begin()){
-    //OLED output "Initing ....."
-    while (1) { delay(10); }
-  }
 
   as7341.setATIME(100);
   as7341.setASTEP(999);
@@ -39,23 +43,27 @@ delay(2000); // Pause for 2 seconds
 
 void loop() {
   // put your main code here, to run repeatedly:
-
   as7341_read();
-
-
+/*
 u8g2.firstPage();
+u8g2.setFont(u8g2_font_6x10_tr);
+  do {
+  show_data();
+  } while ( u8g2.nextPage() );
+ delay(1000);
+*/
+ u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_ncenB10_tr);
     u8g2.drawStr(0,24,"Hello World!");
   } while ( u8g2.nextPage() );
-
 }
 
 
 void  as7341_read(){
 
-  as7341.setLEDCurrent(100); // 100mA
-  as7341.enableLED(true);
+  //as7341.setLEDCurrent(100); // 100mA
+  //as7341.enableLED(true);
   delay(1000);
 if (!as7341.readAllChannels()){
    for(uint8_t i = 0; i < 12; i++) {
@@ -64,8 +72,15 @@ if (!as7341.readAllChannels()){
     // (indices 4 and 5)
     counts[i] = as7341.toBasicCounts(readings[i]);
   }
-}
+  }
   delay(1000);
-  as7341.enableLED(false);
+  //as7341.enableLED(false);
   
+}
+
+
+void show_data(){
+    u8g2.setCursor(0,32);
+    u8g2.print("String test");
+    //u8g2.print(String(counts[1],2));
 }
